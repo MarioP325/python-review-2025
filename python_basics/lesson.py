da375237253b61dfaa7fdebae70612a26bff0618
Pantoja <mapantoja1@cps.k12.il.us>
2025-10-23 14:46:09 -0500

    day1

---
+++ python_basics/lesson.py
@@ -0,0 +1,30 @@
+    # commment
+    # notes for programmers
+    # will not run with the run command
+print("Hello world") # prints message
+    # variables store information for later use
+name="Mario" # Storing a string in a variable
+# string variables are normally enclosed in quotes
+age=15  #Storing an integer in a variable
+# Integers are whole numbers
+height=5.7
+# floats arer numbers with decimal points
+is_student=True
+#booleans represent true/false
+# printing the variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is student:", is_student)
+# concatenating strings is what we do to join variables together
+# its represented by the + signal
+print("Name:", name, "Age:", age, "Height:", height, "Is student:", is_student)
+print("Hello my name is " + name +"" ",I am " + str(age))
+print(f"Hello, my name is {name}, I am {age} years old, my height is {height} feet, and it is {is_student}")
+# create 5 more variables of different types
+city="Chicago"
+temp=64.8
+is_sunny=False
+rain=57
+outside="maybe"
+print(f"Today in {city}, we expect a temperature of {temp}, if you say it is sunny that is {is_sunny}, chance of rain is {rain}, If you want to go outside we would say {outside}")
